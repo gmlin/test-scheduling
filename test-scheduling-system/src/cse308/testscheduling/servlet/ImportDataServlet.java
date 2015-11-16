f42d6dc7440cf203b740be8f83508767d1aadcae
Kenwu <kenneth.wu@stonybrook.edu>
2015-11-16 18:43:32 -0500

    need classID to map students to classes in roster

--- test-scheduling-system/src/cse308/testscheduling/servlet/ImportDataServlet.java
+++ test-scheduling-system/src/cse308/testscheduling/servlet/ImportDataServlet.java
@@ -25,7 +25,9 @@ import javax.servlet.http.Part;
 
 import cse308.testscheduling.Administrator;
 import cse308.testscheduling.Appointment;
+import cse308.testscheduling.Course;
 import cse308.testscheduling.Exam;
+import cse308.testscheduling.Instructor;
 import cse308.testscheduling.Seat;
 import cse308.testscheduling.Student;
 import cse308.testscheduling.User;
@@ -45,27 +47,51 @@ public class ImportDataServlet extends HttpServlet{
 			throws ServletException, IOException{
 		HttpSession s = request.getSession();
 		
-		//String a = request.getParameter("file1");
-		//String b = request.getParameter("file2");
-		//String c = request.getParameter("file3");
-		//System.out.println(a);
-		//System.out.println(b);
-		//System.out.println(c);
-		
 		Part filePart = request.getPart("file1");
 		InputStream fileContent = filePart.getInputStream();
 		BufferedReader reader = new BufferedReader(new InputStreamReader(fileContent));
 		
-		String line;
+		Part filePart2 = request.getPart("file2");
+		InputStream fileContent2 = filePart2.getInputStream();
+		BufferedReader reader2 = new BufferedReader(new InputStreamReader(fileContent2));
+		
+		Part filePart3 = request.getPart("file3");
+		InputStream fileContent3 = filePart3.getInputStream();
+		BufferedReader reader3 = new BufferedReader(new InputStreamReader(fileContent3));
+		
+		Part filePart4 = request.getPart("file4");
+		InputStream fileContent4 = filePart4.getInputStream();
+		BufferedReader reader4 = new BufferedReader(new InputStreamReader(fileContent4));
+		
 		String[] parts;
-		line = reader.readLine();
-		int currentid = 100000003;
+		String line = reader.readLine();
+		String line2 = reader2.readLine();
+		String line3 = reader3.readLine();
+		String line4 = reader4.readLine();
+		
+		
 		
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("test-scheduling-system");
 		EntityManager em = emf.createEntityManager();
 		List<Query> addStudents = new ArrayList<Query>();
 		em.getTransaction().begin();
+		
+		Query queryz = em.createQuery("SELECT student FROM Student student");
+		List<Student> currentStudentList = null;
+		currentStudentList = queryz.getResultList();
+		int currentid = 100000000;
+		try{
+			for(Student student : currentStudentList){
+				if(student.getStudentId() >= currentid){
+					currentid = student.getStudentId();
+				}
+			}
+		}
+		catch(Exception e){
+			
+		}
+		currentid++;
 		
 		try {
 			for (Query query : addStudents)
@@ -76,7 +102,7 @@ public class ImportDataServlet extends HttpServlet{
 		em.getTransaction().commit();
 		em.getTransaction().begin();
 		
-		
+		//add users
 		while ((line = reader.readLine()) != null) {
 			parts = line.split(",");
 			String a = parts[0];
@@ -85,8 +111,8 @@ public class ImportDataServlet extends HttpServlet{
 			String d = parts[3];
 		
 			User newUser = new User();
-			newUser.setFirstName(a);
-			newUser.setLastName(b);
+			newUser.setLastName(a);
+			newUser.setFirstName(b);
 			newUser.setNetId(c);
 			newUser.setEmail(d);
 			newUser.setPassword("123");
@@ -99,7 +125,48 @@ public class ImportDataServlet extends HttpServlet{
 			em.persist(newUser);
         }
 		
-	
+		
+		//add instructors
+		while ((line4 = reader4.readLine()) != null) {
+			parts = line4.split(",");
+			String a = parts[0];
+			String b = parts[1];
+			String c = parts[2];
+			String d = parts[3];
+			
+			User newUser = new User();
+			newUser.setLastName(a);
+			newUser.setFirstName(b);
+			newUser.setNetId(c);
+			newUser.setEmail(d);
+			newUser.setPassword("123");
+			Instructor ins = new Instructor();
+			newUser.setInstructor(ins);
+			ins.setUser(newUser);
+			
+			em.persist(newUser);
+		}
+		
+		
+		/**
+		//add classes ... have to add class ID to table
+		while ((line2 = reader2.readLine()) != null) {
+			parts = line2.split(",");
+			String a = parts[0];
+			String b = parts[1];
+			String c = parts[2];
+			String d = parts[3];
+			String e = parts[4];
+			
+			Course newCourse = new Course();
+			newCourse.setCourseId(a);
+			
+			
+			
+			em.persist(newCourse);
+		}
+		**/
+		
 		
 		
 		em.getTransaction().commit();
